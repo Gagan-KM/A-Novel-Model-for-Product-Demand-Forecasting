8462c2086c2c330f00699c26f627915137398a56
Gagan K M <164402727+Gagan-KM@users.noreply.github.com>
2024-08-15 01:25:32 +0530

    Add files via upload

---
+++ NPFPDF/Guest/Login.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace productDemandForecasting.Guest
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+
+                radiobtnCompany.Checked = true;
+        }
+
+        protected void btnSignin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                BLL obj = new BLL();
+
+                if (txtUserId.Value.Equals("Sony") && txtPassword.Value.Equals("123"))
+                {
+
+
+                    Response.Redirect("~/Company/_SingleBPhone.aspx");
+                }
+                   
+                    else
+                    {
+                        ClientScript.RegisterStartupScript(GetType(), "key", "<script>alert('Invalid id/password!!!')</script>");
+                    }
+                
+                
+            }
+            catch
+            {
+                ClientScript.RegisterStartupScript(GetType(), "key", "<script>alert('Server Error - Check the Database Connectivity!!!')</script>");
+            }
+        }
+    }
+}
